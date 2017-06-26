a698b6250c743c3047e04957e04c1b2175400d1a
Dolvic <Dolvic@users.noreply.github.com>
2017-06-26 17:49:02 -0400

    Add a basic Spring Boot application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.1.3"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.1.3"
+    id("org.springframework.boot") version "1.5.4.RELEASE"
 }
 
 version = "1.0.0"
@@ -10,4 +12,8 @@ repositories {
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib")
+
+    compile("org.springframework.boot:spring-boot-starter")
+    compile("org.springframework.boot:spring-boot-starter-web")
+    compile("org.springframework.boot:spring-boot-starter-actuator")
 }
