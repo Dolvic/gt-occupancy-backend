a1540f4de8715742587c12ad98fb23426f7adef5
Dolvic <Dolvic@users.noreply.github.com>
2017-06-26 17:31:29 -0400

    Setup a basic Kotlin Gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.1.3"
+}
+
+version = "1.0.0"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib")
+}
