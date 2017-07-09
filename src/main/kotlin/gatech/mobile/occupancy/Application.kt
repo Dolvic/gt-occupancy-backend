f7dcae91f572538a26cf8af51de3b1fe12cfdd6d
Dolvic <Dolvic@users.noreply.github.com>
2017-07-09 11:35:18 -0400

    Allow cross origin requests

--- src/main/kotlin/gatech/mobile/occupancy/Application.kt
+++ src/main/kotlin/gatech/mobile/occupancy/Application.kt
@@ -4,12 +4,24 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 class Application
 {
     @Bean
     fun kotlinModule() = KotlinModule()
+
+    @Bean
+    fun corsConfigurer() = object : WebMvcConfigurerAdapter()
+    {
+        override fun addCorsMappings(registry: CorsRegistry)
+        {
+            super.addCorsMappings(registry)
+            registry.addMapping("/**")
+        }
+    }
 
     companion object
     {
