0d1cab413c31d9171d619ffb50406de40d900ab9
Dolvic <Dolvic@users.noreply.github.com>
2017-07-13 05:00:35 -0400

    Replace contains with in operator

--- src/main/kotlin/gatech/mobile/occupancy/startup/BuildingsInitializer.kt
+++ src/main/kotlin/gatech/mobile/occupancy/startup/BuildingsInitializer.kt
@@ -26,28 +26,27 @@ class BuildingsInitializer(
         val (_, accessPoints) = wifiApi.fetchAll(true)
         val wifiBuildings = accessPoints.distinctBy { it.buildingId }
 
-        val mongoBuildings = buildingRepo.findAll()
-        val mongoBuildingIds = mongoBuildings.map { it.buildingId }
+        val mongoBuildingIds = buildingRepo.findAll().map { it.buildingId }
 
-        val codeEntries = codeRepo.findAll()
-        val codes = codeEntries.associateBy(keySelector = { it.buildingId }, valueTransform = { it.code })
+        val codes = codeRepo.findAll().associateBy(keySelector = { it.buildingId }, valueTransform = { it.code })
         val codedIds = codes.keys
 
-        val missingBuildings = missingRepo.findAll()
-        val missingIds = missingBuildings.map { it.buildingId }
+        val missingIds = missingRepo.findAll().map { it.buildingId }
 
-        val newBuildings = wifiBuildings.filterNot { mongoBuildingIds.contains(it.buildingId) }
-                .partition { codedIds.contains(it.buildingId) }
+        val newBuildings = wifiBuildings.filter { it.buildingId !in mongoBuildingIds }
+                .partition { it.buildingId in codedIds }
         val coded = newBuildings.first
         val uncoded = newBuildings.second.toMutableList()
-        uncoded.removeIf { missingIds.contains(it.buildingId) }
+        uncoded.removeIf { it.buildingId in missingIds }
 
-        logger.info { "Found new buildings: ${coded.size} known, ${uncoded.size} unknown" }
+        logger.info { "New buildings: ${coded.size} known, ${uncoded.size} unknown" }
 
         val newMongoBuildings = coded.map { Building(codes.getValue(it.buildingId), it.buildingId, it.buildingName) }
         buildingRepo.save(newMongoBuildings)
+
         val newMissingBuildings = uncoded.map { MissingBuilding(it.buildingId, it.buildingName) }
         missingRepo.save(newMissingBuildings)
-        logger.debug { "New buildings saved." }
+
+        logger.info { "New buildings initialization complete." }
     }
 }
