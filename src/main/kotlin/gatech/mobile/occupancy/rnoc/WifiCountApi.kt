2eea302b152635952cd60f5ea867962b989c1503
Dolvic <Dolvic@users.noreply.github.com>
2017-07-13 11:27:55 -0400

    Handle errors while connecting with Wifi API

--- src/main/kotlin/gatech/mobile/occupancy/rnoc/WifiCountApi.kt
+++ src/main/kotlin/gatech/mobile/occupancy/rnoc/WifiCountApi.kt
@@ -1,19 +1,44 @@
 package gatech.mobile.occupancy.rnoc
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import gatech.mobile.occupancy.OccupancyConfigurationProperties
+import mu.KLogging
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+import java.net.ConnectException
 
 @Component
-class WifiCountApi(clientBuilder: RestTemplateBuilder, appProps: OccupancyConfigurationProperties
-)
+class WifiCountApi(clientBuilder: RestTemplateBuilder,
+                   appProps: OccupancyConfigurationProperties,
+                   private val objectMapper: ObjectMapper)
 {
+    companion object: KLogging()
+
     private val client: RestTemplate = clientBuilder.rootUri(appProps.rnoc.wifiApi)
             .build()
 
     private fun fetchForPath(path: String, withDetails: Boolean = false): WifiCount
-            = client.getForObject("$path/?details=$withDetails", WifiCount::class.java)
+    {
+        try
+        {
+            return client.getForObject("$path/?details=$withDetails", WifiCount::class.java)
+        }
+        catch(ignored: ConnectException)
+        {
+            logger.warn { "Couldn't connect to WiFi API" }
+        }
+
+        val resourceFile = when
+        {
+            "floor" in path -> "floor.json"
+            "building" in path -> "building.json"
+            else -> "all.json"
+        }
+        val resource = javaClass.getResourceAsStream(resourceFile)
+        return objectMapper.readValue<WifiCount>(resource)
+    }
 
     fun fetchAll(withDetails: Boolean = false) = fetchForPath("/", withDetails)
 
